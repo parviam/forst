df2e8b5c72de47aea8dc5621b3a213f06105f4ea
rbokil <rini.bokil@gmail.com>
2025-04-27 15:53:45 -0400

    garden steward signup and list on forum

--- forst/forum/views.py
+++ forst/forum/views.py
@@ -1,12 +1,24 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from .models import Post, Comment, Poll, PollOption
+from .models import Post, Comment, Poll, PollOption, GardenSpace
 from .forms import PostForm, CommentForm, PollForm, PollOptionForm
 from django.contrib.auth.decorators import login_required
+from .forms import GardenSpaceForm
 
 def forum_home(request):
+
     posts = Post.objects.all().order_by('-created_at')
     polls = Poll.objects.all().order_by('-created_at')
-    return render(request, 'forum/forum_home.html', {'posts': posts, 'polls': polls})
+
+    if request.user.is_authenticated:
+        user_gardens = GardenSpace.objects.filter(steward=request.user)
+    else:
+        user_gardens = []
+
+    return render(request, 'forum/forum_home.html', {
+        'posts': posts,
+        'polls': polls,
+        'user_gardens': user_gardens,
+    })
 
 @login_required
 def create_post(request):
@@ -94,4 +106,42 @@ def delete_poll(request, poll_id):
     poll = get_object_or_404(Poll, id=poll_id)
     if request.user == poll.created_by:
         poll.delete()
+    return redirect('forum_home')
+@login_required
+def add_garden_space(request):
+    if request.method == 'POST':
+        form = GardenSpaceForm(request.POST)
+        if form.is_valid():
+            garden = form.save(commit=False)
+            garden.steward = request.user
+            garden.save()
+            return redirect('forum_home')
+    else:
+        form = GardenSpaceForm()
+    
+    return render(request, 'forum/add_garden_space.html', {'form': form})
+@login_required
+def edit_garden(request, pk):
+    # Get the garden object
+    garden = get_object_or_404(GardenSpace, pk=pk)
+
+    # If the request is a POST (form submission), process the form
+    if request.method == 'POST':
+        form = GardenSpaceForm(request.POST, instance=garden)
+        if form.is_valid():
+            form.save()
+            return redirect('forum_home')  # Redirect to the forum home page after saving
+    else:
+        form = GardenSpaceForm(instance=garden)
+
+    return render(request, 'forum/edit_garden.html', {'form': form, 'garden': garden})
+@login_required
+def delete_garden(request, pk):
+    # Get the garden object by primary key (pk)
+    garden = get_object_or_404(GardenSpace, pk=pk)
+
+    # Delete the garden object
+    garden.delete()
+
+    # Redirect the user to the forum home page after deletion
     return redirect('forum_home')
